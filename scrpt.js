a1c6b6c6dec4735ee078dedf352785b11a254bc2
DAXDAY <90981062+DAXDAY@users.noreply.github.com>
2021-10-25 15:47:47 -0300

    java

---
+++ scrpt.js
@@ -0,0 +1,8 @@
+
+let ano_atual = 2021
+let nome = prompt("digite seu nome")
+let nascimento = prompt("digite seu ano de nascimento")
+let idade = ano_atual-nascimento
+let mensagem = `ola,seu nome é ${nome} você tem ${idade}`
+alert(mensagem)
+
